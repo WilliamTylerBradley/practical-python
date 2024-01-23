f51388eb28752eae64df528cd7af60aa524e8872
william.tyler.bradley@gmail.com <william.tyler.bradley@gmail.com>
2024-01-22 19:50:06 -0500

    Chapter 1.3

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 5 * 12
+extra_payment_end_month = 9 * 12
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if extra_payment_start_month <= month and month <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    if principal < 0:
+        overpayment = abs(principal)
+        principal = principal + overpayment
+        total_paid = total_paid - overpayment
+
+    print(month, total_paid, principal)
+    
+print('Total paid', total_paid)
+print('Months', month)
