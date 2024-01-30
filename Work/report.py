e3a0aacdf8bb06ae33fd5996e090b52a64da20ce
william.tyler.bradley@gmail.com <william.tyler.bradley@gmail.com>
2024-01-29 19:19:42 -0500

    Chapter 3.2

--- Work/report.py
+++ Work/report.py
@@ -53,13 +53,14 @@ def make_report(portfolio, prices):
         report.append(row)
     return report
 
-portfolio = read_portfolio('Data\portfoliodate.csv')
-prices = read_prices('Data\prices.csv')
-report = make_report(portfolio, prices)
+def print_report(report):
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {f"${price:.2f}":>10s} {change:>10.2f}')
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %10s %10s' % headers)
-print(('-' * 10 + ' ') * len(headers))
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
 
-for name, shares, price, change in report:
-    print(f'{name:>10s} {shares:>10d} {f"${price:.2f}":>10s} {change:>10.2f}')
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
